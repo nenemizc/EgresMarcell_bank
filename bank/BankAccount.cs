e7970c1d6b761b718a1dd8181544bc4a76db7ce8
Petrik Diak <diak@petrik.hu>
2025-03-28 11:01:05 +0100

    account

---
+++ bank/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+	internal abstract class BankAccount : BankingService
+	{
+		double balance;
+
+		public double Balance { get; }
+
+		protected BankAccount(Owner owner)  : base(owner)
+		{
+		}
+
+		public void Deposit(double amount)
+		{
+			balance += amount;
+		}
+
+		public abstract bool Withdraw(double amount);
+	}
+}
