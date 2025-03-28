ce920e3474d140cc6cab048157619b93e6bdf820
Petrik Diak <diak@petrik.hu>
2025-03-28 10:42:23 +0100

    ohner

---
+++ bank/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+	internal sealed class Owner
+	{
+		string name;
+
+		public Owner(string name)
+		{
+			this.name = name;
+		}
+
+		public string Name { get => name;  }
+	}
+}
